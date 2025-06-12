0865345cbe0eebcdcb20cd7e36835403815b9a86
Yash07-iu <20240802050@dypiu.ac.in>
2025-06-12 14:07:33 +0530

    lang.js

--- lang.js
+++ lang.js
@@ -54,6 +54,28 @@ const translations = {
     mr: {
         // ... existing translations ...
         nav_home: "मुख्यपृष्ठ",
+        "products-title": "विक्रीसाठी उत्पादने",
+        "sno": "क्र.",
+        "product": "उत्पादन",
+        "price": "किंमत",
+        "ghee": "तूप",
+        "buttermilk": "ताक",
+        "biological-letter(jaivik-khat)": "जैविक खात",
+        "vermi-compost(gandul-khat)": "गांडूळ खत",
+        "vermiwash(vermi-wash)": "वर्मीवॉश",
+        "dashparni-ark": "दशपर्णी अर्क",
+        "gomutra": "गोमुत्र",
+        "gokrupamrit": "गोकृपामृत",
+        "cow-dung-cake": "गोवरी",
+        "price-ghee": "२०००/ली",
+        "price-buttermilk": "२०/ली",
+        "price-biological-letter": "२०/कि",
+        "price-vermi-compost": "२५/कि",
+        "price-vermiwash": "६०/ली",
+        "price-dashparni-ark": "१२०/ली",
+        "price-gomutra": "३०/ली",
+        "price-gokrupamrit": "३०/ली",
+        "price-cow-dung-cake": "५/नग",
         // ... existing translations ...
     }
 };
